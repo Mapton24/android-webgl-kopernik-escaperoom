eef6b02393d1d9f8b41d41122495e2a84ac14a94
Mapton24 <robertosk555@gmail.com>
2023-08-11 12:29:21 +0200

    changed rotation to clamp on max angles in playerwalkmobile.cs/implemented button to pick up stuff with camerapointer.cs

--- Assets/Scripts/Game/PlayerScripts/Android/PlayerWalkMobile.cs
+++ Assets/Scripts/Game/PlayerScripts/Android/PlayerWalkMobile.cs
@@ -1,30 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class PlayerWalkMobile : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private float LookDownAngle = 25.0f;
     [SerializeField] private float speed = 3f;
     [SerializeField] private float gravity = 9.81f;
     [SerializeField] private float rotationSpeed = 10f;
     [SerializeField] private CharacterController cc;
+    [SerializeField] private GameObject buttonsPanel;
+    [SerializeField] private float maxLookUpAngle = 80f;
+    [SerializeField] private float maxLookDownAngle = 80f;
 
     bool moveForward = false;
     bool moveBackward = false;
     bool strafeLeft = false;
     bool strafeRight = false;
 
-
-    float minXRotation = -90f;
-    float maxXRotation = 90f;
     float currentXRotation = 0f;
+    bool isPanelNull = false;
 
-
+    private void Awake()
+    {
+        if (buttonsPanel == null)
+            isPanelNull = true;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -72,31 +78,55 @@ public class PlayerWalkMobile : MonoBehaviour
     {
         strafeRight = movePlayer;
     }
-
     private void TouchRotation()
     {
-        if (Input.touchCount > 0)
+        int activeTouchCount = Input.touchCount;
+
+        for (int i = 0; i < activeTouchCount; i++)
         {
-            Touch touch = Input.GetTouch(0);
+            Touch touch = Input.GetTouch(i);
+            if (IsPointerOverUIObject())
+            {
+                Debug.Log("IsTouchingPanel");
+                continue;
+            }
 
-            //if (!EventSystem.current.IsPointerOverGameObject(touch.fingerId))
-            //{
+            float rotationX = -touch.deltaPosition.y * rotationSpeed * Time.deltaTime;
+            float rotationY = touch.deltaPosition.x * rotationSpeed * Time.deltaTime;
 
-                float rotationX = -touch.deltaPosition.y * rotationSpeed * Time.deltaTime;
-                float rotationY = touch.deltaPosition.x * rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up, rotationY, Space.World);
 
-                transform.Rotate(Vector3.up, rotationY, Space.World);
+            currentXRotation += rotationX;
+            currentXRotation = Mathf.Clamp(currentXRotation, -maxLookDownAngle, maxLookUpAngle);
 
-                currentXRotation += rotationX;
+            Quaternion xRotation = Quaternion.Euler(currentXRotation, 0f, 0f);
+            Quaternion yRotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
 
-                Quaternion xRotation = Quaternion.Euler(currentXRotation, 0f, 0f);
-                Quaternion yRotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
+            // Quaternion rotation based on X
+            Quaternion newRotation = yRotation * xRotation;
 
-                // Quaternion rotation based on X
-                Quaternion newRotation = yRotation * xRotation;
-
-                transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
-            //}
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
         }
+    }
+
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        foreach (RaycastResult result in results)
+        {
+            if (!isPanelNull)
+            {
+                if (result.gameObject == buttonsPanel)
+                {
+                    return results.Count > 0;
+                }
+            }
+
+        }
+        return false;
     }
 }
